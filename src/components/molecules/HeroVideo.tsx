3be485d35ef3c620e0d1476ac12580e56e2aada5
Darth Degen <darthdegen@gmail.com>
2025-10-13 08:42:06 -0700

    update video frame aspect ratiog

--- src/components/molecules/HeroVideo.tsx
+++ src/components/molecules/HeroVideo.tsx
@@ -10,7 +10,7 @@ const HeroVideo = () => {
 
   return (
     <div
-      className="full-gold-border relative w-[300px] md:!w-[700px] xl:!w-[700px] aspect-video  bg-black/70 overflow-hidden"
+      className="full-gold-border relative w-[300px] md:!w-[700px] xl:!w-[700px] aspect-[31/18]  bg-black/70 overflow-hidden"
       onClick={() => {
         if (!isPlaying) setPlaying(true);
       }}
